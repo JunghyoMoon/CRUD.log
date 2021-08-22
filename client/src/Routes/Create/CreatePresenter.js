3e194cf8560555072318e2aab9d50e0d06cbd08a
junghyomoon <dsinhome70@gmail.com>
2021-08-22 16:53:30 +0900

    editor is done, todo: Nav style

--- client/src/Routes/Create/CreatePresenter.js
+++ client/src/Routes/Create/CreatePresenter.js
@@ -1,13 +1,39 @@
 import React from "react";
 import styled from "styled-components";
 import Editor from "../../Components/Editor";
+import palette from "../../palette";
 
 const Container = styled.div`
-	padding-top: 100px;
+	padding-top: 80px;
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 `;
 
-const CreatePresenter = () => <Container>
-	<Editor />
-</Container>;
+const Title = styled.h1`
+	font-size: 30px;
+	margin-bottom: 30px;
+`;
+
+const Wrapper = styled.article`
+	width: 95%;
+	padding: 30px;
+	border-radius: 7px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	background-color: ${palette.articleBg};
+`;
+
+const CreatePresenter = () => (
+	<Container>
+		<Wrapper>
+			<Title>Create an Article!</Title>
+			<Editor />
+		</Wrapper>
+	</Container>
+);
 
 export default CreatePresenter;
